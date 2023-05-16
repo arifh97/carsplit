eedaf8b4b358c9a2006582181bc35998c58f9623
imranhwebdev <alimrancv870@gmail.com>
2023-05-17 01:30:29 +0600

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from 'react-bootstrap'
 import checkbox from '../assets/img/checkbox.svg'
 import calenderBg from '../assets/img/calender-bg.svg'
-import Calendar from './Calendar'
+import Calendar from './Calendar';
 import AOS from 'aos';
 
 export default function About(){
     useEffect(()=>{
         AOS.init({duration: 2000})
     }, []);
+    const [collapse, setCollapse] = useState(false);
     return(
         <div className='about pt-130'>
             <Container>
@@ -23,7 +24,13 @@ export default function About(){
                                     </div>
                                     <div className="content">
                                         <h5>Invest</h5>
-                                        <p><span>Investing in Carsplit allows you to own a PART of a supercar, which we then rent out to customers. As an investor, you receive a AMOUNT of the profits from the rental income, providing a unique opportunity to earn passive income from luxury vehicles AND BE A SUPERCARS OWNER  </span></p>
+                                        <p>
+                                            Investing in Carsplit allows you to own a PART of a supercar, which we then rent out to customers. As an investor, you receive a AMOUNT of the profits from the rental income <span className={`long-text ${collapse ? "expanded" : ""}`}>
+                                            providing a unique opportunity to earn passive income from luxury vehicles AND BE A SUPERCARS OWNER 
+                                            </span> 
+                                            <a onClick={() => setCollapse((prev1) => !prev1)}>Read More</a>
+                                        </p>
+                                        
                                     </div>
                                 </li>
                                 <li className="d-flex flex-wrap" data-aos="fade-up" data-aos-duration="1400">
@@ -32,7 +39,12 @@ export default function About(){
                                     </div>
                                     <div className="content">
                                         <h5>We rent</h5>
-                                        <p><span>With Carsplit, you don't have to worry about the hassle of renting out your supercar investment. We take care of everything from 0 TO HUNDRED, including marketing the vehicle, finding qualified renters, managing the rental process, and ensuring that you get YOUR INCOME. Our team of experts handles all the logistics so that you can LEAN back, relax, and enjoy the benefits of your investment without any of the stress or hassle. With Carsplit, investing in a supercar AND BE AN OWNER has never been easier or more profitable.</span> </p>
+                                        <p>
+                                            With Carsplit, you don't have to worry about the hassle of renting out your supercar investment. We take care of everything from 0 to hundred, including marketing the vehicle <span className={`long-text ${collapse ? "expanded" : ""}`}>
+                                            finding qualified renters, managing the rental process, and ensuring that you get YOUR INCOME. Our team of experts handles all the logistics so that you can LEAN back, relax, and enjoy the benefits of your investment without any of the stress or hassle. With Carsplit, investing in a supercar AND BE AN OWNER has never been easier or more profitable.
+                                            </span> 
+                                            <a onClick={() => setCollapse((prev) => !prev)}>Read More</a>
+                                        </p>
                                     </div>
                                 </li>
                                 <li className="d-flex flex-wrap" data-aos="fade-up" data-aos-duration="1600">
@@ -40,8 +52,12 @@ export default function About(){
                                         <object type="image/svg+xml" data={checkbox}></object>
                                     </div>
                                     <div className="content">
-                                        <h5>You get paid</h5>
-                                        <p><span>Imagine POSSESSED A SUPERCAR AND earning money FROM IT ? That's the beauty of Carsplit. As an investor, you can LEAN back and watch your money grow, while we handle all the details of renting out the car and ensuring a seamless experience for customers.</span></p>
+                                        <p>
+                                            Imagine POSSESSED A SUPERCAR AND earning money FROM IT ? That's the beauty of Carsplit. As an investor, you can LEAN back and watch your money grow <span className={`long-text ${collapse ? "expanded" : ""}`}>
+                                            while we handle all the details of renting out the car and ensuring a seamless experience for customers.
+                                            </span> 
+                                            <a onClick={() => setCollapse((prev) => !prev)}>Read More</a>
+                                        </p>
                                     </div>
                                 </li>
                             </ul>
